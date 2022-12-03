97872465d74c544542f22e45c79768b313a8735b
kk10828 <kk10828@gmail.com>
2022-12-03 23:08:31 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        var highestSum = 0
+        input.forEach {
+            if (it.isNotBlank()) sum += it.toInt()
+            if (sum > highestSum) highestSum = sum
+            if (it.isBlank()) sum = 0
+        }
+        return highestSum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val highestSums = mutableListOf(0, 0, 0)
+        var currentSum = 0
+        var minOfHighestSums = 0
+        var minOfHighestSumsIndex = 0
+        var isLastLine = false
+        input.forEachIndexed { index, it ->
+            if (it.isNotBlank()) currentSum += it.toInt()
+            isLastLine = index == input.lastIndex
+            if (it.isBlank() || isLastLine) {
+                minOfHighestSums = highestSums.min()
+                    .also { minSum -> minOfHighestSumsIndex = highestSums.indexOf(minSum) }
+                if (currentSum > minOfHighestSums) highestSums[minOfHighestSumsIndex] = currentSum
+                currentSum = 0
+            }
+        }
+        return highestSums.sum()
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
